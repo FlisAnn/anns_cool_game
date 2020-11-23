3778f0a5d7a77c1e6d2ead0a59f59079d25423ec
flis_ann <ann.flismark@gmail.com>
2020-11-23 21:59:19 +0100

    Removingn unused icon

--- src/components/TopHeader.jsx
+++ src/components/TopHeader.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header, Icon } from 'semantic-ui-react'
+import { Header} from 'semantic-ui-react'
 
 const TopHeader = () => (
   <Header as='h1' textAlign='center' >
